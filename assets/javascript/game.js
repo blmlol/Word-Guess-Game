60ce7a24bdf1498ea037af66debf79a0329a1f3e
Davey Wong <notwdjw@gmail.com>
2019-10-22 14:31:38 -0700

    Added line of code to check is user input is letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,7 @@ var game = {
     guessSlot:'',
     slot: "_ ",
     guessWord: '',
+    alpha: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
     //This function is to input the underscores to demarcate the number of letters in the word to be guessed
     makeSlots: function() {
@@ -44,5 +45,6 @@ var game = {
         this.guessesLeft= this.guessesLeft -1;
         }
     },
+   
 
 };
