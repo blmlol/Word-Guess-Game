0213f046e4bcdb08d4a7c75038287e43d7f75e5d
Davey Wong <notwdjw@gmail.com>
2019-10-22 16:07:32 -0700

    added checkGuess method and put console.log tests for functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,11 @@
-// We need a random number generator function
-//Max is non inclusive; so the input will always be the same number of words that I have
-function getRandom (max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-//Here is my word list with 23 words
-var wordArray = ['jedi', 'lightsaber', 'podracing', 'ewoks', 'lando', 'jabba', 'droid', 'master', 'sithlord', 'chancellor', 'destroyer', 'padme', 'skywalker', 'yoda', 'chewbacca', 'wookie', ' tatooine', 'rebels', 'force', 'senator', 'republic', 'galaxy', 'alderaan' ];
-
-//When I call my random function, it should always be max 23, since it will go from 0-22 which is the range of indexes that my array has
-var guessWord =wordArray[getRandom(23)];
 
 var game = {
     words: ['jedi', 'lightsaber', 'podracing', 'ewoks', 'lando', 'jabba', 'droid', 'master', 'sithlord', 'chancellor', 'destroyer', 'padme', 'skywalker', 'yoda', 'chewbacca', 'wookie', ' tatooine', 'rebels', 'force', 'senator', 'republic', 'galaxy', 'alderaan'],
     isGuessed: false,
     guessesLeft: 5,
     wins: 0,
-    guesses: '',
-    guessSlot:'',
+    //What letters they have guessed
+    guesses: [],
     slot: "_ ",
     guessWord: '',
     alpha: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
@@ -26,23 +15,24 @@ var game = {
         //23 because we have 23 words in our list of possible words
         var num =Math.floor(Math.random() * Math.floor(23));
         this.guessWord = this.words[num];
-        return(this.slot.repeat(this.words[num].length));
+        this.slot= this.slot.repeat(this.words[num].length);
+        
     },
     
     //Function will check user guess to see if it is in the word then proceed to pop up letter(s) if it is in word
     checkGuess: function(guess) {
        if(this.guessWord.includes(guess)){
-            for(var i = 0; i < this.guessWord.length; i+2){
+            for(var i = 0; i < this.guessWord.length; i++){
                 if(guess === this.guessWord[i]){
-                    this.slot.replace(this.slot[i], guess);
+                    this.slot.replace(this.slot[(i * 2)], guess);
                 }
                 
             }
        }
        else
         {
-        this.guessSlot.replace(this.guessSlot[5-game.guessesLeft], guess);
-        this.guessesLeft= this.guessesLeft -1;
+        this.guesses.push(guess);
+        this.guessesLeft= this.guessesLeft - 1;
         }
     },
    
