328d0c42121bfeb1ceb2186df9a43477089ed8cd
Davey Wong <notwdjw@gmail.com>
2019-10-23 12:28:36 -0700

    Fixed checkGuess function to properly display correct guesses on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,34 +7,38 @@ var game = {
     //What letters they have guessed
     guesses: [],
     slot: "_ ",
+    slotArray: [],
     guessWord: '',
     alpha: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
     //This function is to input the underscores to demarcate the number of letters in the word to be guessed
-    makeSlots: function() {
+    makeSlots: function () {
         //23 because we have 23 words in our list of possible words
-        var num =Math.floor(Math.random() * Math.floor(23));
+        var num = Math.floor(Math.random() * Math.floor(23));
         this.guessWord = this.words[num];
-        this.slot= this.slot.repeat(this.words[num].length);
-        
+        this.slot = this.slot.repeat(this.words[num].length);
+        this.slotArray = this.slot.split("")
+
     },
-    
+    //Convert slot string into an array with str.split and then convert back into              
     //Function will check user guess to see if it is in the word then proceed to pop up letter(s) if it is in word
-    checkGuess: function(guess) {
-       if(this.guessWord.includes(guess)){
-            for(var i = 0; i < this.guessWord.length; i++){
-                if(guess === this.guessWord[i]){
-                    this.slot.replace(this.slot[(i * 2)], guess);
+
+    checkGuess: function (guess) {
+        if (this.guessWord.includes(guess)) {
+            //If the guess is included in the word, loop through word and replace underscores with the guessed letter
+            for (var i = 0; i < this.slot.length; i++) {
+                if (guess === this.guessWord[i]) {
+                    this.slotArray[i * 2] = guess;
                 }
-                
             }
-       }
-       else
-        {
-        this.guesses.push(guess);
-        this.guessesLeft= this.guessesLeft - 1;
+            this.slot = this.slotArray.join(" ");
+        }
+        else {
+            this.guesses.push(guess);
+            this.guesses.push(" ");
+            this.guessesLeft = this.guessesLeft - 1;
         }
     },
-   
+
 
 };
